57207acda1b74cdde3e878802e35264cf4b5e662
Dmitry Chebakov <dmitry@chebakov.me>
2021-01-23 22:06:35 +0300

    Использование DB_PASSWORD

--- src/solver/settings.py
+++ src/solver/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bmstu_fun',
         'USER': 'admin',
-        'PASSWORD': 'qwerty',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
